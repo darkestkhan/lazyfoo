a0bc3570050e3f53d7c31487d8b452da42679769
darkestkhan <darkestkhan@gmail.com>
2013-05-12 17:35:40 +0000

    Tut03 from LazyFoo
    Signed-off-by: darkestkhan <darkestkhan@gmail.com>

--- src/util.adb
+++ src/util.adb
@@ -29,11 +29,8 @@ package body Util is
   package TIO renames Ada.Text_IO;
 
   ---------------------------------------------------------------------------
-  -- Current color rendering mode
-  Color_Mode: Integer := Color_Mode_Cyan;
-
   -- Projection Scale
-  Projection_Scale: Float := 1.0;
+  Projection_Scale: constant Float := 1.0;
 
   -- Viewport mode
   Viewport: Viewport_Mode := Full;
@@ -45,7 +42,7 @@ package body Util is
     use type GL.Enum;
   begin
     -- Set the viewport
-    GL.Viewport (0.0, 0.0, Float (Screen_Width), Float (Screen_Height));
+    GL.Viewport (0, 0, Screen_Width, Screen_Height);
 
     -- Initialize Projection matrix
     GL.Matrix_Mode (GL.GL_PROJECTION);
@@ -88,7 +85,6 @@ package body Util is
     GL.Clear (GL.GL_COLOR_BUFFER_BIT);
 
     -- Reset modelview matrix
-    GL.Matrix_Mode (GL.GL_MODELVIEW);
     GL.Load_Identity;
 
     -- Move to center of the screen
@@ -98,29 +94,150 @@ package body Util is
         0.0
       );
 
-    -- Render quad
-    if (Color_Mode = Color_Mode_Cyan) then
+    -- Full View
+    if Viewport = Full then
+      -- Fill the screen
+      GL.Viewport (0, 0, Screen_Width, Screen_Height);
+
+      -- Red Quad
       GL.Begin_Primitive (GL.GL_QUADS);
       begin
-        GL.Color  (Float (  0.0),   1.0,   1.0);
-        GL.Vertex (Float (-50.0), -50.0);
-        GL.Vertex (Float ( 50.0), -50.0);
-        GL.Vertex (Float ( 50.0),  50.0);
-        GL.Vertex (Float (-50.0),  50.0);
+        GL.Color  ( Float (1.0), 0.0, 0.0);
+        GL.Vertex (-Float (Screen_Width) / 2.0, -Float (Screen_Height) / 2.0);
+        GL.Vertex ( Float (Screen_Width) / 2.0, -Float (Screen_Height) / 2.0);
+        GL.Vertex ( Float (Screen_Width) / 2.0,  Float (Screen_Height) / 2.0);
+        GL.Vertex (-Float (Screen_Width) / 2.0,  Float (Screen_Height) / 2.0);
       end;
       GL.End_Primitive;
-    else
-      -- RYGB Mix
+    -- Viewport at center of screen
+    elsif Viewport = Half_Center then
+      -- Center viewport
+      GL.Viewport
+        ( Screen_Width / 4, Screen_Height / 4,
+          Screen_Width / 2, Screen_Height / 2
+        );
+
+      -- Green quad
       GL.Begin_Primitive (GL.GL_QUADS);
       begin
-        GL.Color (Float (1.0), 0.0, 0.0); GL.Vertex (Float (-50.0), -50.0);
-        GL.Color (Float (1.0), 1.0, 0.0); GL.Vertex (Float ( 50.0), -50.0);
-        GL.Color (Float (0.0), 1.0, 0.0); GL.Vertex (Float ( 50.0),  50.0);
-        GL.Color (Float (0.0), 0.0, 1.0); GL.Vertex (Float (-50.0),  50.0);
+        GL.Color  ( Float (0.0), 1.0, 0.0);
+        GL.Vertex (-Float (Screen_Width) / 2.0, -Float (Screen_Height) / 2.0);
+        GL.Vertex ( Float (Screen_Width) / 2.0, -Float (Screen_Height) / 2.0);
+        GL.Vertex ( Float (Screen_Width) / 2.0,  Float (Screen_Height) / 2.0);
+        GL.Vertex (-Float (Screen_Width) / 2.0,  Float (Screen_Height) / 2.0);
+      end;
+      GL.End_Primitive;
+    -- Viewport centered at the top
+    elsif Viewport = Half_Top then
+      -- Viewport at top
+      GL.Viewport
+        ( Screen_Width / 4, Screen_Height / 2,
+          Screen_Width / 2, Screen_Height / 2
+        );
+
+      -- Blue quad
+      GL.Begin_Primitive (GL.GL_QUADS);
+      begin
+        GL.Color  ( Float (0.0), 0.0, 1.0);
+        GL.Vertex (-Float (Screen_Width) / 2.0, -Float (Screen_Height) / 2.0);
+        GL.Vertex ( Float (Screen_Width) / 2.0, -Float (Screen_Height) / 2.0);
+        GL.Vertex ( Float (Screen_Width) / 2.0,  Float (Screen_Height) / 2.0);
+        GL.Vertex (-Float (Screen_Width) / 2.0,  Float (Screen_Height) / 2.0);
+      end;
+      GL.End_Primitive;
+    -- Four viewports
+    elsif Viewport = Quad then
+      -- Bottom left red quad
+      GL.Viewport (0, 0, Screen_Width / 2, Screen_Height / 2);
+      GL.Begin_Primitive (GL.GL_QUADS);
+      begin
+        GL.Color  ( Float (1.0), 0.0, 0.0);
+        GL.Vertex (-Float (Screen_Width) / 4.0, -Float (Screen_Height) / 4.0);
+        GL.Vertex ( Float (Screen_Width) / 4.0, -Float (Screen_Height) / 4.0);
+        GL.Vertex ( Float (Screen_Width) / 4.0,  Float (Screen_Height) / 4.0);
+        GL.Vertex (-Float (Screen_Width) / 4.0,  Float (Screen_Height) / 4.0);
+      end;
+      GL.End_Primitive;
+
+      -- Bottom right green quad
+      GL.Viewport (Screen_Width / 2, 0, Screen_Width / 2, Screen_Height / 2);
+      GL.Begin_Primitive (GL.GL_QUADS);
+      begin
+        GL.Color  ( Float (0.0), 1.0, 0.0);
+        GL.Vertex (-Float (Screen_Width) / 4.0, -Float (Screen_Height) / 4.0);
+        GL.Vertex ( Float (Screen_Width) / 4.0, -Float (Screen_Height) / 4.0);
+        GL.Vertex ( Float (Screen_Width) / 4.0,  Float (Screen_Height) / 4.0);
+        GL.Vertex (-Float (Screen_Width) / 4.0,  Float (Screen_Height) / 4.0);
+      end;
+      GL.End_Primitive;
+
+      -- Top left blue quad
+      GL.Viewport (0, Screen_Height / 2, Screen_Width / 2, Screen_Height / 2);
+      GL.Begin_Primitive (GL.GL_QUADS);
+      begin
+        GL.Color  ( Float (0.0), 0.0, 1.0);
+        GL.Vertex (-Float (Screen_Width) / 4.0, -Float (Screen_Height) / 4.0);
+        GL.Vertex ( Float (Screen_Width) / 4.0, -Float (Screen_Height) / 4.0);
+        GL.Vertex ( Float (Screen_Width) / 4.0,  Float (Screen_Height) / 4.0);
+        GL.Vertex (-Float (Screen_Width) / 4.0,  Float (Screen_Height) / 4.0);
+      end;
+      GL.End_Primitive;
+
+      -- Top right yellow quad
+      GL.Viewport
+        ( Screen_Width / 2, Screen_Height / 2,
+          Screen_Width / 2, Screen_Height / 2
+        );
+      GL.Begin_Primitive (GL.GL_QUADS);
+      begin
+        GL.Color  ( Float (1.0), 1.0, 0.0);
+        GL.Vertex (-Float (Screen_Width) / 4.0, -Float (Screen_Height) / 4.0);
+        GL.Vertex ( Float (Screen_Width) / 4.0, -Float (Screen_Height) / 4.0);
+        GL.Vertex ( Float (Screen_Width) / 4.0,  Float (Screen_Height) / 4.0);
+        GL.Vertex (-Float (Screen_Width) / 4.0,  Float (Screen_Height) / 4.0);
+      end;
+      GL.End_Primitive;
+    -- Viewport with radar subview port
+    elsif Viewport = Radar then
+      -- Fullsize Quad
+      GL.Viewport (0, 0, Screen_Width, Screen_Height);
+      GL.Begin_Primitive (GL.GL_QUADS);
+      begin
+        GL.Color  ( Float (1.0), 1.0, 1.0);
+        GL.Vertex (-Float (Screen_Width) / 8.0, -Float (Screen_Height) / 8.0);
+        GL.Vertex ( Float (Screen_Width) / 8.0, -Float (Screen_Height) / 8.0);
+        GL.Vertex ( Float (Screen_Width) / 8.0,  Float (Screen_Height) / 8.0);
+        GL.Vertex (-Float (Screen_Width) / 8.0,  Float (Screen_Height) / 8.0);
+        GL.Color  ( Float (0.0), 0.0, 0.0);
+        GL.Vertex (-Float (Screen_Width) / 16.0, -Float (Screen_Height) / 16.0);
+        GL.Vertex ( Float (Screen_Width) / 16.0, -Float (Screen_Height) / 16.0);
+        GL.Vertex ( Float (Screen_Width) / 16.0,  Float (Screen_Height) / 16.0);
+        GL.Vertex (-Float (Screen_Width) / 16.0,  Float (Screen_Height) / 16.0);
+      end;
+      GL.End_Primitive;
+
+      -- Radar quad
+      GL.Viewport
+        ( Screen_Width / 2, Screen_Height / 2,
+          Screen_Width / 2, Screen_Height / 2
+        );
+      GL.Begin_Primitive (GL.GL_QUADS);
+      begin
+        GL.Color  ( Float (1.0), 1.0, 1.0);
+        GL.Vertex (-Float (Screen_Width) / 8.0, -Float (Screen_Height) / 8.0);
+        GL.Vertex ( Float (Screen_Width) / 8.0, -Float (Screen_Height) / 8.0);
+        GL.Vertex ( Float (Screen_Width) / 8.0,  Float (Screen_Height) / 8.0);
+        GL.Vertex (-Float (Screen_Width) / 8.0,  Float (Screen_Height) / 8.0);
+        GL.Color  ( Float (0.0), 0.0, 0.0);
+        GL.Vertex (-Float (Screen_Width) / 16.0, -Float (Screen_Height) / 16.0);
+        GL.Vertex ( Float (Screen_Width) / 16.0, -Float (Screen_Height) / 16.0);
+        GL.Vertex ( Float (Screen_Width) / 16.0,  Float (Screen_Height) / 16.0);
+        GL.Vertex (-Float (Screen_Width) / 16.0,  Float (Screen_Height) / 16.0);
       end;
       GL.End_Primitive;
     end if;
 
+    -- Update the screen
     Window.Swap (Win);
   end Render;
 
@@ -130,33 +247,17 @@ package body Util is
     ( Category  : Events.Key_Category;
       Symbol    : Events.Key_Symbol;
       Modifiers : Events.Modifier_Set
-    ) is
+    )
+  is
+    Pragma Unreferenced (Category);
+    Pragma Unreferenced (Modifiers);
   begin
     if Events.To_Character (Symbol) = 'q' then
-      if Color_Mode = Color_Mode_Cyan then
-        Color_Mode := Color_Mode_Multi;
+      if Viewport = Viewport_Mode'Last then
+        Viewport := Viewport_Mode'First;
       else
-        Color_Mode := Color_Mode_Cyan;
+        Viewport := Viewport_Mode'Succ (Viewport);
       end if;
-    elsif Events.To_Character (Symbol) = 'e' then
-      if (Projection_Scale = 1.0) then
-        -- Zoom out
-        Projection_Scale := 2.0;
-      elsif Projection_Scale = 2.0 then
-        -- Zoom in
-        Projection_Scale := 0.5;
-      else
-        -- Regular zoom
-        Projection_Scale := 1.0;
-      end if;
-
-      -- Update projection matrix
-      GL.Matrix_Mode (GL.GL_PROJECTION);
-      GL.Load_Identity;
-      GL.Ortho
-        ( 0.0, GL.Double (Float (Screen_Width) * Projection_Scale),
-          GL.Double (Float (Screen_Height) * Projection_Scale), 0.0, 1.0, -1.0
-        );
     end if;
     Terminated := Events.To_Character (Symbol) = ASCII.ESC;
   end Key_Press;
